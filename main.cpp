c0bd52855481cdff704fb7d6e7f4cd29e7d312bd
Vadim <ttupachill123@gmail.com>
2024-05-08 14:30:59 +0400

    Iterative calculation of Fibonacci numbers

--- main.cpp
+++ main.cpp
@@ -1,18 +1,32 @@
 #include <iostream>
 using namespace std;
 
-int fib(int n){
-	if (n == 1 || n == 2)
-		return n-1;
-	return fib (n-1) + fib (n-2);
+void fib(int n) { //Функция вычисления чисел Фибоначчи
+    int a = 0;
+    int b = 1;
+    
+    if (n <= 0) {
+        return;
+    }
+    
+    cout << "Первые " << n << " чисел Фибоначчи:\n";
+    
+    for (int i = 0; i < n; ++i) {
+        cout << a << " ";
+        int next = a + b;
+        a = b;
+        b = next;
+    }
 }
 
-int main(){
-	setlocale (LC_ALL,"RUS");
-	int n;
-	cout << "n =";
-	cin>>n;
-	
-	cout<<"Результат: "<<fib(n)<<endl;
-	return 0;
+int main() {
+    setlocale (LC_ALL,"RUS"); //Испоьзование русской клавиатуры
+    int n;
+    cout << "Введите количество чисел Фибоначчи для вычисления: ";
+    cin >> n;
+    
+    fib(n); //Вызов функции
+    
+    return 0; //Завершение работы функции
 }
+
