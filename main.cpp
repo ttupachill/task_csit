52afac75cae915f376e63471fc94297e0943d747
Vadim <ttupachill123@gmail.com>
2024-05-08 13:36:45 +0400

    add function fib

--- main.cpp
+++ main.cpp
@@ -1,8 +1,18 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
-    cout<<"Hello World!"<<endl;
-    return 0;
+int fib(int n){
+	if (n == 1 || n == 2)
+		return 1;
+	return fib (n-1) + fib (n-2);
+}
+
+int main(){
+	setlocale (LC_ALL,"RUS");
+	int n;
+	cout << "n =";
+	cin>>n;
+	
+	cout<<"Результат: "<<fib(n)<<endl;
+	return 0;
 }
